a5793c1ea4f68ab49a0f9d90f25a9cd05c1992bf
Enrique Devars <e.devars.dev@gmail.com>
2020-02-23 15:14:18 -0600

    Aggregando una mejor grafica de codificacion

--- codificar.m
+++ codificar.m
@@ -1,6 +1,7 @@
-function [r,c, dicc] = codificar(F_x, xn)
-    c = zeros(64,1);
-    r = zeros(64,1);
+function [xn_c,Fx_c, dicc] = codificar(F_x, xn)
+    
+    xn_c = zeros(64,1);
+    Fx_c = zeros(64,1);
     j = 0;
     
     for i = -32:32
@@ -8,42 +9,38 @@ function [r,c, dicc] = codificar(F_x, xn)
             j=j+1;
             n = i./32;
             
-            b1 = "";
-            b2 = "";
-            b3 = "";
             
-            % Codificacion del bit de nivel
+            % Codificacion del bit de polaridad
             if i >= 1
                 b1 = "1";
             else
                 b1 = "0";
             end
             
+            
             s = ceil(abs(i)/8);
            
+            % Codificacion de los bit de segmento
+            b2 = dec2bin(s-1,2);
             
-            if s == 1
-                b2 = "00";
-            elseif s == 2
-                b2 = "01";
-            elseif s == 3
-                b2 = "10";
-            else 
-                b2 = "11";
-            end
             
-            numDec = abs(i) - s.*8 + 7; 
+            numDec = abs(i) - s.*8 + 7;
             
+            
+            % Codificacion de los bits de nivel dentro del segmento
             b3 = dec2bin(numDec,3);
             
             
-            
+            % Creacion de los niveles de F_x y referencia de las amplitudes
+            % de xn
             k = find(abs(F_x-n) < 0.01);
             [~,idx]=min(abs(k-n));
             minIndex=k(idx);
-            c(j) = F_x(minIndex);
-            r(j) = xn(minIndex);
+            Fx_c(j) = F_x(minIndex);
+            xn_c(j) = xn(minIndex);
             dicc(j) = strcat(b1,b2,b3);
         end
     end
+    
+    dicc = transpose(dicc);
 end
